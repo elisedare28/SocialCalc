b3541c74178daed56c1af8f1e5317498ad690f8e
Arunima Banerjee <125211920+elisedare28@users.noreply.github.com>
2024-09-01 17:23:23 +0530

    Update Signup.jsx

--- socialcalc/src/components/Signup.jsx
+++ socialcalc/src/components/Signup.jsx
@@ -20,7 +20,7 @@ export default function Signup() {
     }
 
     try {
-      const response = await axios.post("https://social-calc-api.vercel.app/api/users/signup", {
+      const response = await axios.post("/api/users/signup", {
         email,
         password,
         name
