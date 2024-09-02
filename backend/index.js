e45b5f8efed7bbfd4c8fb8fcfe07a725e52c0941
Arunima Banerjee <125211920+elisedare28@users.noreply.github.com>
2024-09-02 13:32:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,9 +29,14 @@ app.use(cors({
 app.use(bodyParser.json());
   // Serve static files if needed
 app.use(express.static('public')); 
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.get('/', (req, res) => {
